5905d43f84e2661e33fa55bf03ec220749e83434
agaghd <cover_story@163.com>
2020-03-22 18:38:53 +0800

    Add kawaii bot

---
+++ kawaii_bot.js
@@ -0,0 +1,20 @@
+var kawaiiBot = function (msg, count, delay) {
+    if (count <= 0) {
+        return
+    }
+    if (!delay || delay < 1000) {
+        delay = 1000
+    }
+    var chatInput = document.getElementsByClassName('chat-input')[0]
+    chatInput.value = msg
+    var chatInputChangeEvent = new InputEvent('input')
+    chatInput.dispatchEvent(chatInputChangeEvent)
+    var sendBtn = document.getElementsByClassName('bl-button live-skin-highlight-button-bg bl-button--primary bl-button--small')[0]
+    setTimeout(function () {
+        sendBtn.click()
+        count--
+        setTimeout(function () {
+            kawaiiBot(msg, count, delay)
+        }, delay)
+    }, 1000)
+}
---
+++ kawaii_bot_import.js
@@ -0,0 +1,5 @@
+var script=document.createElement("script");
+script.type="text/javascript";
+script.src="https://agaghd.github.io/js/kawaii_bot.js";
+document.getElementsByTagName('head')[0].appendChild(script); 
+alert("Kawaii Bot 装载完毕")
